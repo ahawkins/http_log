b2a8b627086a954c9cb9ca72e87aae89372d7137
twinturbo <me@broadcastingadam.com>
2012-05-01 19:10:14 -0700

    Fix precompling test

--- test/integration/precompiling_test.rb
+++ test/integration/precompiling_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
-class LoggingTest < ActionDispatch::IntegrationTest
+class PrecompilingTest < ActionDispatch::IntegrationTest
   def test_http_log_js_is_precompilable
     assert_includes Rails.application.config.assets.precompile, 'http_log.js'
+    assert_includes Rails.application.config.assets.precompile, 'http_log.css'
   end
 end
