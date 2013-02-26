a8b71b112b90d6eebf5929c0979170818cba13b0
twinturbo <me@broadcastingadam.com>
2013-02-26 15:57:05 +0100

    Remove pride

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,7 +4,7 @@ ENV["RAILS_ENV"] = "test"
 require File.expand_path("../dummy/config/environment.rb",  __FILE__)
 require "rails/test_help"
 
-require 'minitest/pride'
+require 'minitest/autorun'
 
 Rails.backtrace_cleaner.remove_silencers!
 
